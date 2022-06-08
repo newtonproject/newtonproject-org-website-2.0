4488fd116a9a31872410f719e3cba6c9a71afe94
liukeke <liukeke@diynova.com>
2022-06-08 16:10:40 +0800

    feat: banner

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -24,35 +24,59 @@ let bannerlist = [
     }
 ];
 
-const Banner = () => (
-    <div className={'banner'}>
-        <div className={'banner-centent'}>
-            <div className={'banner-news'}>
-                <p className='mb-2'>Newton Weekly<span>03/07/2022 - 03/13 • 03/4</span></p>
-                <p>Mining Instructions<span>14/04/2021</span></p>
-                <div className={'banner-title'}>
-                    Infrastructure<br />For The<br />Community<br />Economy
+export default function () {
+    return (
+        <>
+            <div className={'banner-pc'}>
+                <div className={'banner-centent'}>
+                    <div className={'banner-news'}>
+                        <p className='mb-2'>Newton Weekly<span>03/07/2022 - 03/13 • 03/4</span></p>
+                        <p>Mining Instructions<span>14/04/2021</span></p>
+                        <div className={'banner-title'}>
+                            Infrastructure<br />For The<br />Community<br />Economy
+                        </div>
+                    </div>
+                    <div className={'banner-img'}>
+                        <img src={banner} />
+                    </div>
+                </div>
+                <div className={'banner-link'}>
+                    {
+                        bannerlist.map((item, index) => {
+                            return (
+                                <Link to={item.url} key={index}>
+                                    <span>{item.title}</span>
+                                    <i>{item.centent}</i>
+                                </Link>
+                            )
+                        })
+                    }
                 </div>
             </div>
-            <div className={'banner-img'}>
-                <img src={banner} />
+            <div className={'banner-mobile'}>
+                <div className={'banner-title'}>
+                    Infrastructure<br />For The<br />Community Economy
+                </div>
+                <div className={'banner-img'}>
+                    <img src={banner} />
+                </div>
+                <div className={'banner-news'}>
+                    <p className='mb-2'>Newton Weekly<span>03/07/2022 - 03/13 • 03/4</span></p>
+                    <p>Mining Instructions<span>14/04/2021</span></p>
+                </div>
+                <div className={'banner-link'}>
+                    {
+                        bannerlist.map((item, index) => {
+                            return (
+                                <Link to={item.url} key={index}>
+                                    <span>{item.title}</span>
+                                    <i>{item.centent}</i>
+                                </Link>
+                            )
+                        })
+                    }
+                </div>
             </div>
-
-        </div>
-
-        <div className={'banner-link'}>
-            {
-                bannerlist.map((item, index) => {
-                    return (
-                        <Link to={item.url} key={index}>
-                            <span>{item.title}</span>
-                            <i>{item.centent}</i>
-                        </Link>
-                    )
-                })
-            }
-        </div>
-    </div>
-)
-
-export default Banner
+        </>
+    )
+}
