0a80296cc43e3c301cf980ac139b666a8418e997
liukeke <liukeke@diynova.com>
2022-06-09 16:51:37 +0800

    fix: margin

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -7,25 +7,31 @@ const News = () => (
             <h2>News</h2>
             <ul>
                 <li>
-                    <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/news.png' /></div>
-                    <div className={'news-title'}>
-                        <h3>The Black Men in White Coats Youth Summxs White Coats Youth</h3>
-                        <p>March 16, 2022</p>
-                    </div>
+                    <a href='https://www.centralcharts.com/en/1224903-newton-new-usdt/analysis/144706-newton-new-usdt-daily' target='_blank'>
+                        <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/home/news1.jpg' /></div>
+                        <div className={'news-title'}>
+                            <h3>NEWTON - NEW/USDT - Daily - Technical analysis published on 02/15/2021</h3>
+                            <p>February 16, 2021</p>
+                        </div>
+                    </a>
                 </li>
                 <li>
-                    <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/home/build-img.png' /></div>
-                    <div className={'news-title'}>
-                        <h3>The Black Men in White Coats The Black Men in White CoatsThe Black Men in White CoatsThe Black Men in White Coats</h3>
-                        <p>March 16, 2022</p>
-                    </div>
+                    <a href='https://mp.weixin.qq.com/s/sQItJfBIT0d8DgfqhaFAMA' target='_blank'>
+                        <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/home/news2.jpg' /></div>
+                        <div className={'news-title'}>
+                            <h3>币源AMA之NewSwap流动性挖矿能带来什么</h3>
+                            <p>February 04, 2021</p>
+                        </div>
+                    </a>
                 </li>
                 <li>
-                    <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/home/stake-img.png' /></div>
-                    <div className={'news-title'}>
-                        <h3>The Black Men in White Coats Youth Sum</h3>
-                        <p>March 16, 2022</p>
-                    </div>
+                    <a href='https://www.jinse.com/news/blockchain/691074.html' target='_blank'>
+                        <div className={'news-img'} ><StaticImage alt='img' src='../../static/images/home/news3.jpg' /></div>
+                        <div className={'news-title'}>
+                            <h3>基于牛顿基础设施的区块链发票之落地进展</h3>
+                            <p>May 27, 2020</p>
+                        </div>
+                    </a>
                 </li>
             </ul>
         </div>
