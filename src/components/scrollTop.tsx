3ea1e3b50734f16ab0627eca20ecd362eca8c902
liukeke <liukeke@diynova.com>
2022-06-16 13:46:48 +0800

    fix: scroll top

--- src/components/scrollTop.tsx
+++ src/components/scrollTop.tsx
@@ -4,24 +4,26 @@ import { createThrottle } from '../hooks/createThrottle'
 
 export default function ScrollTop() {
 
-    const [show, switchShow] = useState(false);
-    useEffect(()=>{
-      const listener = createThrottle(()=>{
-        if (window.scrollY > window.innerHeight) {
-            switchShow(true);
-        } else {
-            switchShow(false);
-        }  
-      }, 500) as EventListener;
-      document.addEventListener('scroll', listener);
-      return () => document.removeEventListener('scroll', listener);
-    }, [show])
-    
-    return show ? (
-        <div onClick={() => window.scrollTo(0, 0)} className={'scroll-top'}>
-          <StaticImage className={'img-box'} alt='scroll-top' src='../static/images/scroll-top.png' />
-        </div>
-    ): null
+  const [show, switchShow] = useState(false);
+  useEffect(() => {
+    const listener = createThrottle(() => {
+      if (window.scrollY > window.innerHeight) {
+        switchShow(true);
+      } else {
+        switchShow(false);
+      }
+    }, 500) as EventListener;
+    document.addEventListener('scroll', listener);
+    return () => document.removeEventListener('scroll', listener);
+  }, [show])
+
+  return show ? (
+    <div id={'scroll'}>
+      <div onClick={() => window.scrollTo(0, 0)} className={'scroll-top'}>
+        <StaticImage className={'img-box'} alt='scroll-top' src='../static/images/scroll-top.png' />
+      </div>
+    </div>
+  ) : null
 }
 
 
