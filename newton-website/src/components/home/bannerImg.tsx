17752a82162f7b8b71206acedfa28160d913971f
liukeke <liukeke@diynova.com>
2022-06-29 20:41:40 +0800

    fix: img

--- newton-website/src/components/home/bannerImg.tsx
+++ newton-website/src/components/home/bannerImg.tsx
@@ -3,15 +3,24 @@ import bannerMobileImg from '../../static/images/banner.png'
 import bannerMobileImgDev from '../../static/images/banner-dev.png'
 
 export default function BannerImg() {
-  const [loaded, setLoaded] = useState(false)
-  const onLoad = useCallback(() => {
-    setLoaded(true)
-  }, [])
+  // const [loaded, setLoaded] = useState(false)
+  // const onLoad = useCallback(() => {
+  //   setLoaded(true)
+  // }, [])
+
+  const [imgSrc, setImgSrc] = useState('')
+  function onLoad() {
+    setImgSrc(bannerMobileImg)
+  }
+  function onError() {
+    setImgSrc(bannerMobileImgDev)
+  }
 
   return (
     <>
-      <img style={{ display: loaded ? 'block' : 'none' }} onLoad={onLoad} src={bannerMobileImg} />
-      {!loaded && <img src={bannerMobileImgDev} />}
+      {/* <img style={{ display: loaded ? 'block' : 'none' }} onLoad={onLoad} src={bannerMobileImg} />
+      {!loaded && <img src={bannerMobileImgDev} />} */}
+      <img onLoad={onLoad} onError={onError} src={imgSrc} />
     </>
   )
 }
