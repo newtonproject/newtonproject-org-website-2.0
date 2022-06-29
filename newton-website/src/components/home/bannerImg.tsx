e892cf76bab7bbb5b3aa32b5a7f38e1b505a2f59
liukeke <liukeke@diynova.com>
2022-06-29 19:32:57 +0800

    fix: img

--- newton-website/src/components/home/bannerImg.tsx
+++ newton-website/src/components/home/bannerImg.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import bannerMobileImg from '../../static/images/banner.png'
 import bannerMobileImgDev from '../../static/images/banner-dev.png'
 
 export default function BannerImg() {
   const [loaded, setLoaded] = useState(false)
-  function onLoad() {
+  const onLoad = useCallback(() => {
     setLoaded(true)
-  }
+  }, [])
 
   return (
     <>
-      {loaded == false ? <img src={bannerMobileImgDev} /> : null}
       <img onLoad={onLoad} src={bannerMobileImg} />
+      {!loaded && <img src={bannerMobileImgDev} />}
     </>
   )
 }
