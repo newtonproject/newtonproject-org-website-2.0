76f2520c02f6494521769889b859c30b0abe87e4
liukeke <liukeke@diynova.com>
2022-06-29 19:52:23 +0800

    fix: imgage

--- newton-website/src/components/home/bannerImg.tsx
+++ newton-website/src/components/home/bannerImg.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 import bannerMobileImg from '../../static/images/banner.png'
 import bannerMobileImgDev from '../../static/images/banner-dev.png'
 
 export default function BannerImg() {
   const [loaded, setLoaded] = useState(false)
-  const onLoad = useCallback(() => {
+  function onLoad() {
     setLoaded(true)
-  }, [])
+  }
 
   return (
     <>
-      <img onLoad={onLoad} src={bannerMobileImg} />
+      <img style={{ display: loaded ? 'block' : 'none' }} onLoad={onLoad} src={bannerMobileImg} />
       {!loaded && <img src={bannerMobileImgDev} />}
     </>
   )
