ac110071f6dd3396086ec56af66624ce895c1143
liukeke <liukeke@diynova.com>
2022-06-30 15:00:45 +0800

    fix: banner

--- newton-website/src/components/home/bannerImg.tsx
+++ newton-website/src/components/home/bannerImg.tsx
@@ -3,16 +3,16 @@ import bannerMobileImg from '../../static/images/banner.png'
 import bannerMobileImgDev from '../../static/images/banner-dev.png'
 
 export default function BannerImg() {
-  const [loaded, setLoaded] = useState(false)
+  const [bannerImg, setBannerImg] = useState(bannerMobileImgDev)
   const time = new Date().getTime()
   const onLoad = useCallback(() => {
-    setLoaded(true)
+    setBannerImg(bannerMobileImg)
   }, [])
 
   return (
     <>
-      <img style={{ display: loaded ? 'block' : 'none' }} onLoad={onLoad} src={bannerMobileImg + '?' + time} />
-      {!loaded && <img src={bannerMobileImgDev + '?' + time} />}
+      <img onLoad={onLoad} src={bannerImg} />
+      {/* {!loaded && <img src={bannerMobileImgDev + '?' + time} />} */}
     </>
   )
 }
