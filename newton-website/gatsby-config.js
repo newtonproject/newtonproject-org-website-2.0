961f7a57c9b5523c7398e888245f9c857142b403
weixuefeng <weixuefeng@diynova.com>
2022-07-04 17:20:13 +0800

    fix: change redirect to false

--- newton-website/gatsby-config.js
+++ newton-website/gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
         defaultLanguage: `en`,
         // redirect to `/${lang}/` when connecting to `/`
         // based on user's browser language preference
-        redirect: true,
+        redirect: false,
         redirectComponent: require.resolve(`./src/components/redirect.js`),
       },
     },
